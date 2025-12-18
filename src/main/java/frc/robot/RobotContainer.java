52510aeb05011e07f8ff894bdab6f55461667601
DynamicWhiteHat <aahil.syed.usa@gmail.com>
2025-12-18 17:52:39 -0500

    Added skeleton

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class RobotContainer {
+    public RobotContainer() {
+        configureSubsystems();
+        configureCommands();
+        configureControllers();
+        configureBindings();
+    }
+
+    public void configureSubsystems() {}
+    public void configureCommands() {}
+    public void configureControllers() {}
+    public void configureBindings() {}
+}
